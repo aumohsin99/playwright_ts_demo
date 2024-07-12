4908ef7ce41bbd27dc1e658cfacee754f55b03aa
Ata Ul Mohsin <ataulmohsin1999@gmail.com>
2024-07-12 15:28:55 +0500

    setup again

--- tests/talha_portfolio.spec.ts
+++ tests/talha_portfolio.spec.ts
@@ -1,6 +1,9 @@
 import { test, expect } from "@playwright/test";
 import exp from "node:constants";
+import { randomInt } from "node:crypto";
 import { beforeEach, describe } from "node:test";
+//import {Math} from
+//import {Math} from randomInt
 
 //variables
 let xpathofabout: string = '//*[@id="root"]/div[2]/div[1]/nav/div/ul/li[1]/a';
@@ -8,6 +11,8 @@ let xpathofaboutmousebutton: string =
   '//*[@id="root"]/div[2]/div[1]/section/div[3]/a';
 let xpathofwork: string = '//*[@id="root"]/div[2]/div[1]/nav/div/ul/li[2]/a';
 let xpathofcontact: string = '//*[@id="root"]/div[2]/div[1]/nav/div/ul/li[3]/a';
+let xpathofprojectscontainer: string =
+  '//*[@id="root"]/div[2]/section[4]/div[3]';
 
 test("correct URL pointed and page title displayed", async ({ page }) => {
   await page.goto("https://mtalham.netlify.app/");
@@ -68,4 +73,20 @@ test("position moved to contact heading correctly using navbar link", async ({
   await page.locator(`xpath=${xpathofcontact}`).click();
   await expect(page).toHaveURL("https://mtalham.netlify.app/#contact");
   await expect(page.getByRole("heading", { name: "Contact" })).toBeVisible();
+});
+
+test("scrolled down to projects, random github link clicked and page is opened", async ({
+  page,
+}) => {
+  await page.goto("https://mtalham.netlify.app/");
+  await expect(page.getByRole("heading", { name: "Projects" })).toBeVisible();
+
+  let indexofproject: number = randomInt(0 - 5);
+
+  //let arrayofprojectsotbetested: string[] = [];
+  //access main heading of projects
+
+  let randomxpathofprojectscontainer:string=xpathofprojectscontainer+
+
+  await page.locator(`${}+`)
 });
